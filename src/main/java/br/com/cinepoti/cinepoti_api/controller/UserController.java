d0b8453c41a303d97258adc0d50db85795222bc3
Gabriel Victor <gabrieltedz.com.@gmail.com>
2024-12-17 20:57:34 -0300

    feat: update user

--- src/main/java/br/com/cinepoti/cinepoti_api/controller/UserController.java
+++ src/main/java/br/com/cinepoti/cinepoti_api/controller/UserController.java
@@ -1,18 +1,20 @@
 package br.com.cinepoti.cinepoti_api.controller;
 
-import br.com.cinepoti.cinepoti_api.model.user.UserRequestDTO;
-import br.com.cinepoti.cinepoti_api.model.user.UserResponseDTO;
+import br.com.cinepoti.cinepoti_api.dto.request.UserRequestDTO;
+import br.com.cinepoti.cinepoti_api.dto.response.UserResponseDTO;
 import br.com.cinepoti.cinepoti_api.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
-@RequestMapping("/users")  // Define o caminho base para os endpoints relacionados ao usuário
+@RequestMapping("/usuarios")  // Define o caminho base para os endpoints relacionados ao usuário
+@CrossOrigin
+
 public class UserController {
 
 
@@ -23,9 +25,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    // Endpoint para criar um novo usuário
-    @PostMapping
-    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO userRequestDTO) {
+    // Endpoint para registrar um novo usuário
+    @PostMapping("/cadastrar")
+    public ResponseEntity<UserResponseDTO> createUser(@Valid  @RequestBody UserRequestDTO userRequestDTO) {
         // Chama o serviço para salvar o usuário
         UserResponseDTO userResponseDTO = userService.saveUser(userRequestDTO);
 
@@ -42,7 +44,7 @@ public class UserController {
 
     // Endpoint para buscar um usuário por ID
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable("id") UUID id) {
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable("id") Long id) {
         UserResponseDTO userResponseDTO = userService.getUserById(id);
         if (userResponseDTO != null) {
             return new ResponseEntity<>(userResponseDTO, HttpStatus.OK);
@@ -53,7 +55,7 @@ public class UserController {
     // Endpoint para atualizar um usuário
     @PutMapping("/{id}")
     public ResponseEntity<UserResponseDTO> updateUser(
-            @PathVariable("id") UUID id,
+            @PathVariable("id") Long id,
             @RequestBody UserRequestDTO userRequestDTO) {
 
         UserResponseDTO userResponseDTO = userService.updateUser(id, userRequestDTO);
@@ -61,6 +63,14 @@ public class UserController {
             return new ResponseEntity<>(userResponseDTO, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+
+
     }
 
 }
--- src/main/java/br/com/cinepoti/cinepoti_api/model/user/User.java
+++ src/main/java/br/com/cinepoti/cinepoti_api/model/user/User.java
@@ -1,91 +1,159 @@
 package br.com.cinepoti.cinepoti_api.model.user;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import br.com.cinepoti.cinepoti_api.enums.Gender;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.io.Serializable;
-import java.util.UUID;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
-@Table(name = "tb_user")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Table(name = "CP_USER")
+
 public class User implements Serializable {
 
     @Id
-    @GeneratedValue
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @NotNull(message = "Username is required")
-    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
-    private String userName;
+    @NotNull(message = "Login is required")
+    @Size(min = 3, max = 50, message = "login must be between 3 and 50 characters")
+    @Column(nullable = false, unique = true)
+    private String login;
 
-    @NotNull(message = "Email is required")
-    @Email(message = "Invalid email format")
-    private String email;
 
     @NotNull(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters long")
+    @Size(min = 8, message = "Password must be at least 6 characters long")
+    @Column(nullable = false)
     private String password;
 
 
-    public User(String userName, String email, String password) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-    }
+    @NotNull(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Column(nullable = false, unique = true)
+    private String email;
 
-    public User(UUID id,
-                @NotNull(message = "Username is required")
-                @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters") String userName,
-                @NotNull(message = "Email is required") @Email(message = "Invalid email format")
-                String email, @NotNull(message = "Password is required")
-                @Size(min = 6, message = "Password must be at least 6") String password){
+    @Column(nullable = true)
+    private String name;
+
+    @Column(nullable = true, unique = true, length = 11)
+    @Pattern(regexp = "\\d{11}", message = "CPF must have 11 digits")
+    private String cpf;
+
+    @Pattern(regexp = "\\d{10,15}", message = "Telephone must be between 10 and 15 digits")
+    @Column(nullable = true)
+    private String telephone;
+
+    @Column(nullable = true)
+    @Enumerated(EnumType.STRING) // Armazenando o nome do enum no banco
+    private Gender gender;
+
+    @Column(nullable = true)
+    private LocalDate birthDate;
+
+    public User() {}
+
+    public User(Long id, LocalDate birthDate, Gender gender, String cpf, String telephone, String name, String email, String password, String login) {
         this.id = id;
-        this.userName = userName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.cpf = cpf;
+        this.telephone = telephone;
+        this.name = name;
         this.email = email;
         this.password = password;
+        this.login = login;
     }
 
+    public String getLogin() {
+        return login;
+    }
 
-    public UUID getId() {
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(UUID id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public @NotNull(message = "Username is required") @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters") String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(@NotNull(message = "Username is required") @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters") String userName) {
-        this.userName = userName;
-    }
-
-    public @NotNull(message = "Email is required") @Email(message = "Invalid email format") String getEmail() {
-        return email;
-    }
-
-    public void setEmail(@NotNull(message = "Email is required") @Email(message = "Invalid email format") String email) {
-        this.email = email;
-    }
-
-    public @NotNull(message = "Password is required") @Size(min = 6, message = "Password must be at least 6 characters long") String getPassword() {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(@NotNull(message = "Password is required") @Size(min = 6, message = "Password must be at least 6 characters long") String password) {
+    public void setPassword (String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf (String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
