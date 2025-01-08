6c5811bd43ef3188c51c3ae22ab2ac5116eff75d
Gabriel Victor <gabrieltedz.com.@gmail.com>
2025-01-07 22:22:38 -0300

    feat: create mappers and enums

---
+++ src/main/java/br/com/cinepoti/cinepoti_api/enums/PaymentMethod.java
@@ -0,0 +1,7 @@
+package br.com.cinepoti.cinepoti_api.enums;
+
+public enum PaymentMethod {
+    CARD,
+    PIX,
+    CASH
+}
---
+++ src/main/java/br/com/cinepoti/cinepoti_api/enums/PaymentStatus.java
@@ -0,0 +1,7 @@
+package br.com.cinepoti.cinepoti_api.enums;
+
+public enum PaymentStatus {
+    PAID,
+    PENDING,
+    FAILED
+}
