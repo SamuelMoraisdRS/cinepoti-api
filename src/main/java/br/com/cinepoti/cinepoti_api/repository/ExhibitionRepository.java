1180206e9caa0af0bb0f4d408fc1bbe95ad0d7ae
Samuel Morais <SamuelMoraisdRS>
2025-01-15 18:31:12 -0300

    Feat: findByCinemaRoomAndStartTime query method in ExhibitionRepository

--- src/main/java/br/com/cinepoti/cinepoti_api/repository/ExhibitionRepository.java
+++ src/main/java/br/com/cinepoti/cinepoti_api/repository/ExhibitionRepository.java
@@ -1,7 +1,14 @@
 package br.com.cinepoti.cinepoti_api.repository;
 
+import br.com.cinepoti.cinepoti_api.model.CinemaRoom;
 import br.com.cinepoti.cinepoti_api.model.Exhibition;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
+
+  Optional<Exhibition> findByCinemaRoomAndStartTime(CinemaRoom cinemaRoom, LocalDateTime startTime);
 }
