9bdce53d37fa9faf57218bfaf8ed8aef8cfb7bed
Samuel Morais <SamuelMoraisdRS>
2025-01-15 18:35:23 -0300

    feat: data.sql configuration, for testing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,13 @@
---INSERT INTO CP_USER (login, password, email, name, cpf, telephone, gender, birth_date)
---VALUES
---  ('user1', 'password123', 'user1@example.com', 'User One', '12345678901', '11988887777', 'MALE', '1995-03-15'),
---  ('user2', 'password456', 'user2@example.com', 'User Two', '98765432109', '11977776666', 'FEMALE', '1998-06-20'),
---  ('user3', 'password789', 'user3@example.com', 'User Three', '11122233344', '11966665555', 'OTHER', '2001-11-10'),
---  ('user4', 'password456', 'user4@example.com', null, '11122233346', null, null, null);
+INSERT INTO CP_ADDRESS (street, city, state, neighborhood, street_number, cep)
+VALUES ('Main Street', 'Springfield', 'Illinois', 'Downtown', '123', '62704-123');
+
+INSERT INTO cp_MOVIE (title, duration, release_date, rating, synopsis)
+VALUES ('Inception', 148, '2010-07-16', 8.8, 'A skilled thief is given a chance at redemption if he can successfully perform inception.');
+
+INSERT INTO CP_CINEMA_ROOM (name, address_id)
+VALUES
+( 'Cinema Room 1', 1);
+
+INSERT INTO CP_SEAT (id, seat_row, seat_number, cinema_room_id, price)
+VALUES
+(1, 'A', '1', 1, 15.50);
