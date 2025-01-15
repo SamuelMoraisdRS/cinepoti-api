a88648a30560294f56973b64c8d241cddede3b21
Samuel Morais <SamuelMoraisdRS>
2025-01-15 17:56:13 -0300

    feat: Ticket and Booking service

---
+++ src/main/java/br/com/cinepoti/cinepoti_api/service/BookingService.java
@@ -0,0 +1,159 @@
+package br.com.cinepoti.cinepoti_api.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import br.com.cinepoti.cinepoti_api.dto.request.BookingRequestDTO;
+import br.com.cinepoti.cinepoti_api.dto.request.PaymentRequestDTO;
+import br.com.cinepoti.cinepoti_api.dto.request.TicketRequestDTO;
+import br.com.cinepoti.cinepoti_api.dto.response.BookingResponseDTO;
+import br.com.cinepoti.cinepoti_api.dto.response.PaymentResponseDTO;
+import br.com.cinepoti.cinepoti_api.enums.BookingStatus;
+import br.com.cinepoti.cinepoti_api.enums.PaymentStatus;
+import br.com.cinepoti.cinepoti_api.mapper.BookingMapper;
+import br.com.cinepoti.cinepoti_api.mapper.PaymentMapper;
+import br.com.cinepoti.cinepoti_api.mapper.TicketMapper;
+import br.com.cinepoti.cinepoti_api.model.Booking;
+import br.com.cinepoti.cinepoti_api.model.Exhibition;
+import br.com.cinepoti.cinepoti_api.model.Payment;
+import br.com.cinepoti.cinepoti_api.model.Seat;
+import br.com.cinepoti.cinepoti_api.model.Ticket;
+import br.com.cinepoti.cinepoti_api.model.User;
+import br.com.cinepoti.cinepoti_api.repository.BookingRepository;
+
+@Service
+public class BookingService {
+
+  private final BookingRepository bookingRepository;
+  private final UserService userService;
+  private final ExhibitionService exhibitionService;
+  private final SeatService seatService;
+  private final TicketService ticketService;
+  private final PaymentService paymentService;
+
+  public BookingService(BookingRepository bookingRepository, UserService userService,
+      ExhibitionService exhibitionService, SeatService seatService, TicketService ticketService, PaymentService paymentService) {
+    this.bookingRepository = bookingRepository;
+    this.userService = userService;
+    this.exhibitionService = exhibitionService;
+    this.seatService = seatService;
+    this.ticketService = ticketService;
+    this.paymentService = paymentService;
+  }
+
+  /*
+   * Métodos que retornam DTOs são exclusivos aos controllers, os que retornam
+   * instancias das entidades são usados nas outras camadas.
+   */
+
+  public BookingResponseDTO create(BookingRequestDTO bookingDTO, Long userId) {
+
+    User user = userService.getUserObjectById(userId);
+
+    // Consideramos que a exibicao e sala ja existem na database
+    Exhibition exhibition = exhibitionService.getExhibitionObjById(bookingDTO.exhibitionId());
+
+    // Recuperar as instancias de Seats. Supondo que já existem no banco de dados
+    Map<Long, Seat> ticketsSeats = new HashMap<>();
+
+    // Map de Seats necessários para instanciar os novos tickets
+    for (Entry<Long, TicketRequestDTO> entry : bookingDTO.tickets().entrySet()) {
+      Seat seat = seatService.getObjById(entry.getValue().seatId());
+      ticketsSeats.put(seat.getId(), seat);
+    }
+
+    Booking booking = BookingMapper.toEntity(bookingDTO, user, exhibition, ticketsSeats);
+
+    bookingRepository.save(booking);
+
+    for (Ticket ticket : booking.getTickets().values()) {
+      ticketService.create(ticket);
+    }
+
+    return BookingMapper.toResponseDTO(booking);
+
+  }
+
+  public List<BookingResponseDTO> getAllUsersBookings(Long userId) {
+    return bookingRepository.findAllByUserId(userId).get().stream()
+        .map((booking) -> BookingMapper.toResponseDTO(booking)).toList();
+  }
+
+  public BookingResponseDTO getBookingById(Long bookingId) {
+    return BookingMapper.toResponseDTO(bookingRepository.findById(bookingId).orElseThrow());
+  }
+
+  public Booking getBookingObjById(Long bookingId) {
+    return bookingRepository.findById(bookingId).orElseThrow();
+  }
+
+  public BookingResponseDTO updateBooking(Long id, BookingRequestDTO bookingDTO) {
+    // Instancia atual do objeto Booking
+    Booking updatedBooking = bookingRepository.findById(id).orElseThrow();
+
+    // Novo Map de tickets do booking
+    // Se os tickets do novo booking tem mesmo ID do antigo, consideramos que não
+    // houveram alterações nos tickets
+    if (bookingDTO.tickets().keySet().equals(updatedBooking.getTickets().keySet())) {
+      Map<Long, Ticket> newTickets = new HashMap<>();
+
+      bookingDTO.tickets().values().stream().forEach((ticketDTO) -> {
+        Seat seat = seatService.getObjById(ticketDTO.seatId());
+        Ticket newTicket = TicketMapper.toEntity(ticketDTO, updatedBooking, seat);
+        newTickets.put(newTicket.getId(), newTicket);
+        ticketService.create(newTicket);
+      });
+      updatedBooking.setTickets(newTickets);
+    }
+
+    updatedBooking.setTotalAmount(calculateTotalPrice(updatedBooking));
+    updatedBooking.setBookingDate(bookingDTO.bookingDate());
+    bookingRepository.save(updatedBooking);
+    return BookingMapper.toResponseDTO(updatedBooking);
+  }
+
+  @PutMapping
+  public BookingResponseDTO cancelBooking(Long id) {
+    Booking booking = bookingRepository.findById(id).orElseThrow();
+    booking.setStatus(BookingStatus.CANCELLED);
+    bookingRepository.save(booking);
+    return BookingMapper.toResponseDTO(booking);
+  }
+
+  public BookingResponseDTO cancelTicket(Long ticketId) {
+    Ticket ticket = ticketService.getTicketObjById(ticketId);
+    Booking booking = bookingRepository.findById(ticket.getBooking().getId()).orElseThrow();
+    booking.getTickets().remove(ticketId);
+    ticketService.deleteTicket(ticketId);
+    bookingRepository.save(booking);
+    return BookingMapper.toResponseDTO(booking);
+  }
+
+  public PaymentResponseDTO getBookingPayment(Long bookingId) {
+    return PaymentMapper.toResponseDTO(bookingRepository.findById(bookingId).orElseThrow().getPayment());
+  }
+
+  public BookingResponseDTO pay(Long bookingId, PaymentRequestDTO paymentDTO) {
+    Booking booking = getBookingObjById(bookingId);
+    Payment payment = PaymentMapper.toEntity(paymentDTO, booking, booking.getTotalAmount());
+    payment.setPaymentStatus(PaymentStatus.PAID);
+    paymentService.create(payment);
+    booking.setStatus(BookingStatus.CONFIRMED);
+    booking.setPayment(payment);
+    bookingRepository.save(booking);
+    return BookingMapper.toResponseDTO(booking);
+  }
+
+  public Double calculateTotalPrice(Booking booking) {
+    Double totalPrice = 0.;
+    for (Ticket ticket : booking.getTickets().values()) {
+      totalPrice += ticket.getSeat().getPrice();
+    }
+    return totalPrice;
+  }
+}
